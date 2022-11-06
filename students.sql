69e92cf931a365c6dbe4e152ed0a2a25faa2eec2
Tharanigit123 <108514187+Tharanigit123@users.noreply.github.com>
2022-11-06 21:06:02 +0530

    Update students.sql

--- students.sql
+++ students.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 11, 2021 at 01:03 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.29
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,10 +11,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `students`
---
-
--- --------------------------------------------------------
+-- -
 
 --
 -- Table structure for table `attendence`
